274e309ac04ec573a022d376aa7d3a03b5c91c0c
Aniket <aniketmitra123456789@gmail.com>
2026-06-09 01:17:43 +0530

    feat: define LangGraph AgentState and initialize Gemini LLM

---
+++ app/agent/nodes.py
@@ -0,0 +1,15 @@
+import os
+from langchain_google_genai import ChatGoogleGenerativeAI
+from dotenv import load_dotenv
+
+load_dotenv()
+
+gemini_api_key = os.getenv("GEMINI_API_KEY")
+if not gemini_api_key:
+    raise ValueError("GEMINI_API_KEY is not set in the environment variables.")
+
+llm = ChatGoogleGenerativeAI(
+    model="gemini-1.5-pro",
+    temperature=0,
+    api_key=gemini_api_key
+)
---
+++ app/agent/state.py
@@ -0,0 +1,10 @@
+from typing import TypedDict, Annotated, Sequence
+import operator
+from langchain_core.messages import BaseMessage
+
+class AgentState(TypedDict):
+    messages: Annotated[Sequence[BaseMessage], operator.add]
+
+    issue_description: str
+    fetched_code: str
+    final_fix: str
